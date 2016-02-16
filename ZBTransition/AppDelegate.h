267dae06c30a9fca204d17fae4e67ada91cc6da2
WuZhengBin <zhengbin.wu@shuyin.cc>
2016-02-16 10:23:10 +0800

    Initial Commit

---
+++ ZBTransition/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZBTransition
+//
+//  Created by WuZhengBin on 16/2/16.
+//  Copyright © 2016年 WuZhengBin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
